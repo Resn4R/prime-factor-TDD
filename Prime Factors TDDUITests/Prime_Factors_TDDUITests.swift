9db61575ce13e809dfd6f738afddd29102c3fa2b
Resn4R <purview-joust.0x@icloud.com>
2023-11-23 14:44:40 +0000

    one test passed

--- Prime Factors TDDUITests/Prime_Factors_TDDUITests.swift
+++ Prime Factors TDDUITests/Prime_Factors_TDDUITests.swift
@@ -6,14 +6,7 @@
 //
 
 import XCTest
-@testable import Prime_Factors_TDD
 
 final class Prime_Factors_TDDUITests: XCTestCase {
-    func testGivenInvalidInputWhengetPrimeFactorsReturnNeg1() {
-        let calculator = Calculator()
-        
-        let sample = 0
-        
-        XCTAssertEqual(calculator.getPrimeFactors(of: sample), [-1])
-    }
+
 }
