6c3042d1ec687c6e2c1d923dfd385383593e60de
Resn4R <purview-joust.0x@icloud.com>
2023-11-23 14:40:02 +0000

    first commit

---
+++ Prime Factors TDD/Number.swift
@@ -0,0 +1,14 @@
+//
+//  Number.swift
+//  Prime Factors TDD
+//
+//  Created by Vito Borghi on 23/11/2023.
+//
+
+import Foundation
+
+struct Calculator {
+    func getPrimeFactors(of number: Int) -> [Int] {
+        [-1]
+    }
+}
